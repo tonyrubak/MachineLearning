ef78eb058aea428189ff75e8c5a34e6ac8f95c23
Joe <joe@crimsonhexagon.us>
2018-04-14 15:42:09 -0400

    added function to compute logistic log-likelihood

--- src/Regression.jl
+++ src/Regression.jl
@@ -14,3 +14,15 @@ end
 function feature_deriv_logistic(errors, feature)
     dot(errors, feature)
 end
+
+function compute_log_likelihood_logistic(model, features, output)
+    ind = map(x -> (x == 1) ? 1 : 0, output)
+    scores = features * model.β
+    logexp = log.(1. + exp.(-scores))
+
+    mask = isinf.(logexp)
+    logexp[mask] = -scores[mask]
+
+    sum((ind - 1) .* scores - logexp)
+end
+
