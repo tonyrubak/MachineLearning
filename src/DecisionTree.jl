7b6cb4e2fc63ae9d2f60b9ff6490a6d9e2a61e7b
Joe <joe@crimsonhexagon.us>
2018-05-05 23:03:11 -0400

    added weighted decision trees and boosting

--- src/DecisionTree.jl
+++ src/DecisionTree.jl
@@ -2,7 +2,7 @@ module DecisionTree
 using DataFrames
 using DataFramesMeta
 
-export decision_tree_create, classify, complexity, evaluate
+export weighted_decision_tree_create, decision_tree_create, classify, complexity, evaluate, adaboost
 
 abstract type TreeNode{T} end
 
@@ -214,7 +214,7 @@ function classify(tree::BranchNode, x, annotate = false)
     end
 end
 
-function evaluate{T}(tree::TreeNode{T}, data, target)
+function evaluate(tree::TreeNode, data, target)
     n = size(data)[1]
     preds = map(x -> classify(tree, x), eachrow(data))
     error = preds .* data[:,target]
@@ -234,4 +234,146 @@ end
     # @test node_mistakes([-1, -1, 1, 1, 1, 1]) == 2
     # @test node_mistakes([-1, -1, -1, -1, -1, 1, 1]) == 2
 # end
+
+### Weighted Decision Tree
+
+function node_mistakes_weighted(labels, weights)
+    wt_positive = any(labels .== 1) ? sum(weights[labels .== 1]) : 0
+    wt_mistakes_neg = wt_positive
+    wt_negative = any(labels .== -1) ? sum(weights[labels .== -1]) : 0
+    wt_mistakes_pos = wt_negative
+
+
+    if wt_mistakes_pos <= wt_mistakes_neg
+        (wt_mistakes_pos, 1)
+    else
+        (wt_mistakes_neg, -1)
+    end
+end
+
+function weighted_splitting_feature(data, features, target, weights)
+    best_feature = :none
+    best_error = Inf
+    n = float(size(data)[1])
+
+    for feature in features
+        left_split = data[data[feature] .== 0,:]
+        right_split = data[data[feature] .== 1,:]
+        left_wts = weights[data[feature] .== 0,:]
+        right_wts = weights[data[feature] .== 1,:]
+        error = (node_mistakes_weighted(left_split, left_wts)[1] +
+            node_mistakes_weighted(right_split, right_wts)[1]) /
+            sum(weights)
+        if error < best_error
+            best_feature = feature
+            best_error = error
+        end
+    end
+    best_feature 
+end
+
+function create_leaf(values, weights)
+    weigted_error, best_class = node_mistakes_weighted(values, weights)
+    LeafNode(best_class)
+end
+
+function weighted_decision_tree_create(data, features, target, weights,
+                              current_depth = 0, max_depth = 10)
+    remaining_features = copy(features)
+    target_values = data[:, target]
+    n = size(data)[1]
+
+    println("--------------------------------------------------------------------")
+    println("Subtree, depth = $current_depth ($(length(target_values)) data points).")
+
+    # Check for the two base stopping conditions
+    # 1. Error is 0
+    # 2. There are no more features to split on
+    # and the early stopping conditions
+    # 1. The tree has reached the maximum depth
+    if node_mistakes_weighted(data[:, target], weights)[1] <= 1e-15
+        println("Stopping condition 1 reached.")
+        return create_leaf(target_values, weights)
+    elseif remaining_features == []
+        println("Stopping condition 2 reached.")
+        return create_leaf(target_values, weights)
+    elseif current_depth ≥ max_depth
+        println("Reached maximum depth. Stopping for now.")
+        return create_leaf(target_values, weights)
+    end
+
+    splitting_feature = weighted_splitting_feature(data, features,
+                                                   target, weights)
+    delete!(remaining_features, splitting_feature)
+    
+    left_split = data[data[splitting_feature] .== 0,:]
+    left_weights = weights[data[splitting_feature] .== 0,:]
+    right_split = data[data[splitting_feature] .== 1,:]
+    right_weights = weights[data[splitting_feature] .== 1,:]
+
+    println("Splitting on $splitting_feature")
+    if size(left_split)[1] == n
+        println("Creating leaf node")
+        return create_leaf(left_split[:, target], weights)
+    elseif size(right_split)[1] == n
+        println("Creating leaf node")
+        return create_leaf(right_split[:, target], weights)
+    end
+    
+    left_tree = weighted_decision_tree_create(left_split,
+                                              remaining_features,
+                                              target, left_weights,
+                                              current_depth + 1,
+                                              max_depth)
+    right_tree = weighted_decision_tree_create(right_split,
+                                               remaining_features,
+                                               target, right_weights,
+                                               current_depth + 1,
+                                               max_depth)
+    return BranchNode(splitting_feature, left_tree, right_tree)
+end
+
+## Boosting
+function adaboost(data, features, target, num_stumps)
+    alpha = repeat([1.], inner = size(data)[1])
+    weights = []
+    tree_stumps = []
+    values = data[:,target]
+
+    for t in 1:num_stumps
+        println("=====================================================")
+        println("Adaboost Iteration $t")
+        println("=====================================================")
+
+        stump = weighted_decision_tree_create(data, features, target,
+                                              alpha, 0, 1)
+        push!(tree_stumps, stump)
+
+        preds = map(x -> classify(stump, x), eachrow(data))
+
+        correct = preds .== values
+        incorrect = preds .≠ values
+
+        weighted_error = sum(alpha[incorrect])/sum(alpha)
+        weight = 0.5 * log((1-weighted_error)/weighted_error)
+        push!(weights, weight)
+
+        adjustment = map(x -> x ? exp(-weight) : exp(weight), correct)
+        alpha = alpha .* adjustment
+        s_alpha = sum(alpha)
+        alpha = alpha ./ s_alpha
+    end
+    weights, tree_stumps
+end
+
+function predict(weights, stumps, data)
+    scores = zeros(Float64, size(data)[1])
+    
+    for (i, stump) in enumerate(stumps)
+        preds = map(x -> classify(stump, x), eachrow(data))
+        scores = scores + weights[i] .* preds
+    end
+
+    map(x -> x > 0 ? 1 : -1, scores)
+end
 end
