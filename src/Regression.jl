ae0be12005662e86e32fc141df5be0fdeac5ddec
Joe <joe@crimsonhexagon.us>
2018-05-10 23:47:39 -0400

    made broadcasting more broadcasty

--- src/Regression.jl
+++ src/Regression.jl
@@ -7,9 +7,8 @@ struct Model{T<:Real}
 end
 
 function predict_logistic(features, w)
-    preds = similar(w)
     wth = features * w
-    1 ./ (1+exp.(-wth))
+    1 ./ (1 .+ exp.(-wth))
 end
 
 function feature_deriv_logistic(errors, feature)
@@ -24,14 +23,14 @@ function compute_log_likelihood_logistic(model, features, output)
     mask = isinf.(logexp)
     logexp[mask] = -scores[mask]
 
-    sum((ind - 1) .* scores - logexp)
+    sum((ind .- 1) .* scores .- logexp)
 end
 
 function fit_logistic(feature_matrix, output, w, step_size, max_iter)
     indicators = map(x -> (x == 1) ? 1 : 0, output)
     for iter in 1:max_iter
         preds = predict_logistic(feature_matrix, w)
-        errors = indicators - preds
+        errors = indicators .- preds
         for j in eachindex(w)
             deriv = feature_deriv_logistic(errors, feature_matrix[:, j])
             w[j] = w[j] + step_size * deriv 
